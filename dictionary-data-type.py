db0e977b484aab5958b9d203c8c614b67afe36e1
Tomal <tanjimahmed450@gmail.com>
2026-09-20 17:32:21 +0600

    add 2 files

---
+++ dictionary-data-type.py
@@ -0,0 +1,14 @@
+#creating dictionary
+
+employee_directory = {
+    "EMP101" : "Tanjim",
+    "EMP102" : "Tomal",
+    "EMP103" : "Ahmed"
+}
+print(employee_directory["EMP101"])
+
+employee_directory['EMP104']= "Nehal"
+print(employee_directory)
+
+employee_directory["EMP101"] = "Tanjim Ahmed"
+print(employee_directory["EMP101"])
