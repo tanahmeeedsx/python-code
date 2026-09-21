29530ca55327ea6e4c4b5c9fba423f0601d3137e
Tomal <tanjimahmed450@gmail.com>
2026-09-21 20:21:01 +0600

    add a file3

---
+++ conditions.py
@@ -0,0 +1,9 @@
+age = 16
+has_job = False
+#if, elif, else
+if age >= 18:
+    print('you can get married')
+elif has_job == True:
+    print('you can get married too')
+else:
+    print('Ruko jara, Sabr karo')
