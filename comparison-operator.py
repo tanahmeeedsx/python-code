c2f78a53c8c49041a65ad085bdc09c19fc897dd7
Tomal <tanjimahmed450@gmail.com>
2026-09-21 13:57:51 +0600

    add a file

---
+++ comparison-operator.py
@@ -0,0 +1,15 @@
+# comparison operators in python
+x = 10
+y = 20
+# Equal to
+print(x == y)  # False
+# Not equal to
+print(x != y)  # True
+# Greater than
+print(x > y)  # False
+# Less than
+print(x < y)  # True
+# Greater than or equal to
+print(x >= y)  # False
+# Less than or equal to
+print(x <= y)  # True   
